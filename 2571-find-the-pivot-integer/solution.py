3dad1ef43cb4efb1adffdc7637666ba22c4942ac
sailu chikkam <chikkamsailu@gmail.com>
2026-06-04 06:18:09 +0000

    Sync LeetCode submission Runtime - 7 ms (59.03%), Memory - 19.3 MB (21.61%)

---
+++ 2571-find-the-pivot-integer/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def pivotInteger(self, n: int) -> int:
+        total = sum(range(1, n + 1))
+
+        left = 0
+
+        for i in range(1, n + 1):
+            left += i
+
+            if left == total - left + i:
+                return i
+
+        return -1
+        

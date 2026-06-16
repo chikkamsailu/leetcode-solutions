0553e8a69fb29970fa61b0e29e7d04c6856b2984
sailu chikkam <chikkamsailu@gmail.com>
2026-06-16 06:21:50 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.2 MB (51.39%)

---
+++ 3476-find-minimum-operations-to-make-all-elements-divisible-by-three/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minimumOperations(self, nums):
+        count = 0
+
+        for num in nums:
+            if num % 3 != 0:
+                count += 1
+
+        return count
+        

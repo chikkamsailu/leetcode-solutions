dcb94749bc14ca115ed08be2b25a763ab2f338c1
sailu chikkam <chikkamsailu@gmail.com>
2026-06-23 04:29:51 +0000

    Sync LeetCode submission Runtime - 37 ms (63.96%), Memory - 24.9 MB (23.29%)

---
+++ 0969-number-of-recent-calls/solution.py
@@ -0,0 +1,12 @@
+class RecentCounter:
+
+    def __init__(self):
+        self.calls = []
+
+    def ping(self, t):
+        self.calls.append(t)
+
+        while self.calls[0] < t - 3000:
+            self.calls.pop(0)
+
+        return len(self.calls)

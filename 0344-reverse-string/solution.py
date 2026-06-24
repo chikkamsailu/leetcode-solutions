196c4568d5667adcdcae738e6cd1d5d160af9915
sailu chikkam <chikkamsailu@gmail.com>
2026-06-24 04:15:08 +0000

    Sync LeetCode submission Runtime - 7 ms (10.43%), Memory - 23.3 MB (97.36%)

--- 0344-reverse-string/solution.py
+++ 0344-reverse-string/solution.py
@@ -1,13 +1,8 @@
-from typing import List
-
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        """
-        Do not return anything, modify s in-place instead.
-        """
-        left, right = 0, len(s) - 1
-        while left < right:
-            s[left], s[right] = s[right], s[left]  # swap
-            left += 1
-            right -= 1
-
+        l = 0
+        r = len(s) - 1
+        while l < r:
+            s[l], s[r] = s[r], s[l]   # swap
+            l += 1                    # move left pointer
+            r -= 1                    # move right pointer

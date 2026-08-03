62e8fddd9456ba5c68540e72aa611b4d8456e7bd
sailu chikkam <chikkamsailu@gmail.com>
2026-08-03 14:11:12 +0530

    Time: 0 ms (100%), Space: 19.4 MB (42.06%) - LeetHub - Time: O(m * n), Space: O(m * n)

---
+++ 0062-unique-paths/0062-unique-paths.py
@@ -0,0 +1,13 @@
+class Solution:
+    def uniquePaths(self, m, n):
+        dp = [[1] * n for _ in range(m)]
+
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+
+        return dp[m - 1][n - 1]
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
